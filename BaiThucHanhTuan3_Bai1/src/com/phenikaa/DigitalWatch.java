3f5c5c59e518a6a79f0f7a1690ffd2193b1ad968
Phong2002 <phongkingvn@gmail.com>
2022-04-25 15:09:52 +0700

    commit BTH3 B1

---
+++ BaiThucHanhTuan3_Bai1/src/com/phenikaa/DigitalWatch.java
@@ -0,0 +1,53 @@
+package com.phenikaa;
+
+import javax.swing.*;
+import java.awt.*;
+import java.text.*;
+import java.util.*;
+
+public class DigitalWatch implements Runnable {
+
+    JFrame f;
+    Thread t = null;
+    String timeString = "";
+    JButton b;
+
+
+    DigitalWatch() {
+        f = new JFrame();
+
+        t = new Thread(this);
+        t.start();
+
+        b = new JButton();
+        b.setBounds(100, 100, 100, 50);
+        b.setBackground(Color.pink);
+        f.add(b);
+        f.setSize(300, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                SimpleDateFormat formatter = new SimpleDateFormat("hh:mm:ss");
+//                Date date = cal.getTime();
+                timeString = formatter.format(new Date());
+
+                printTime();
+
+                t.sleep(1000);  // interval given in milliseconds
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void printTime() {
+        b.setText(timeString);
+    }
+
+    public static void main(String[] args) {
+        new DigitalWatch();
+    }
+}
---
+++ BaiThucHanhTuan3_Bai1/src/com/phenikaa/Main.java
@@ -0,0 +1,8 @@
+package com.phenikaa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DigitalWatch digitalWatch = new DigitalWatch();
+    }
+}
